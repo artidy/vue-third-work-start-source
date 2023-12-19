695cae73425068a1d9d218d5495d2060722862b8
Andrey Pechennikov <ap@devandy.me>
2023-12-19 16:19:26 +0600

    Add layouts

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button> - </button>
+
+    <span> Текущее количество: 0 </span>
+
+    <button> + </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
