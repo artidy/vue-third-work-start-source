2f35fe9ac5a7660338dc9ee54fcdbd9b8bb6498c
Andrey Pechennikov <ap@devandy.me>
2023-12-19 18:04:00 +0600

    Add counter

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,14 +1,26 @@
 <script setup>
+import { ref } from 'vue'
 
+const count = ref(0)
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1
+}
 </script>
 
 <template>
-  <div>
-    <button> - </button>
+  <div class="counter">
+    <button @click="decrement"> - </button>
 
-    <span> Текущее количество: 0 </span>
+    <span> Текущее количество: {{ count }} </span>
 
-    <button> + </button>
+    <button @click="increment"> + </button>
   </div>
 </template>
 
