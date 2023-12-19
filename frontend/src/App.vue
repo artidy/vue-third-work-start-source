827c67c06b597c57313da9797e99efbffda1e14e
Andrey Pechennikov <ap@devandy.me>
2023-12-19 20:49:10 +0600

    Add styles for project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,42 +11,4 @@ import { AppLayout } from "@/layouts";
 
 <style lang="scss">
 @import "@/assets/scss/app.scss";
-
-#app {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background-color: $blue-600;
-  top: 0;
-  left: 0;
-}
-
-.main {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: $white-900;
-
-  img {
-    display: block;
-    margin: 0 auto;
-  }
-
-  h1 {
-    @include m-s36-h21;
-    text-align: center;
-    margin-bottom: 0;
-  }
-
-  p {
-    font-size: 20px;
-    line-height: 30px;
-    text-align: center;
-  }
-
-  b {
-    font-size: 1.2em;
-  }
-}
 </style>
