20ea4a4d9b5f1d994ea8a13618f600cb6a02f208
Andrey Pechennikov <ap@devandy.me>
2023-12-21 11:35:17 +0600

    Change layouts

---
+++ frontend/src/layouts/AppLayoutDefault.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+</script>
+
+<template>
+  <div class="app_layout">
+    <app-layout-header />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
---
+++ frontend/src/layouts/AppLayoutMain.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AppLayoutSidebar from "@/layouts/AppLayoutSidebar.vue";
+import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+  filters: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(["updateTasks"]);
+</script>
+
+<template>
+  <div class="app_layout">
+    <app-layout-header />
+    <div class="content">
+      <app-layout-sidebar
+          :tasks="props.tasks"
+          @update-tasks="$emit('updateTasks', $event)"
+      />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
